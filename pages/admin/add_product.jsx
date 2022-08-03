d106df0fbb6eae023af17c18fdd06a8cc113f68f
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-03 13:22:08 +0700

    add dummy view pages

---
+++ pages/admin/add_product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function add_product() {
+  return <div>add_product</div>;
+}
+
+export default add_product;
---
+++ pages/admin/history_order.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function history_order(props) {
+  return <div>history_order</div>;
+}
+
+history_order.propTypes = {};
+
+export default history_order;

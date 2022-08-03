ffad0c461534d812291405d07ed47cd8b6c609a8
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-03 15:49:33 +0700

    add loading for data fetching process

--- pages/admin/history_order.jsx
+++ pages/admin/history_order.jsx
@@ -7,6 +7,7 @@ import { AdminHO } from "../../components/Card";
 
 function home_admin() {
   const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchData();
@@ -42,46 +43,51 @@ function home_admin() {
           setDatas(result.data);
         }
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => console.log("error", error))
+      .finally(() => setLoading(false));
   };
-  return (
-    <div className="h-screen w-screen bg-[#eeee]">
-      <Header />
-      <div className="my-10 md:my-14 lg:my-20 flex space-x-5 justify-center">
-        <h1 className="text-black text-center text-base font-bold md:text-4xl lg:text-4xl">
-          All Users Order History
-        </h1>
-        <Link href="./home_admin">
-          <button className="p-1 bg-[#557EBC] text-white rounded-md text-sm md:text-2xl lg:text-4xl">
-            Back
-          </button>
-        </Link>
-      </div>
+  if (loading) {
+    return <div>Please wait...</div>;
+  } else {
+    return (
+      <div className="h-screen w-screen bg-[#eeee]">
+        <Header />
+        <div className="my-10 md:my-14 lg:my-20 flex space-x-5 justify-center">
+          <h1 className="text-black text-center text-base font-bold md:text-4xl lg:text-4xl">
+            All Users Order History
+          </h1>
+          <Link href="./home_admin">
+            <button className="p-1 bg-[#557EBC] text-white rounded-md text-sm md:text-2xl lg:text-4xl">
+              Back
+            </button>
+          </Link>
+        </div>
 
-      <div className="ml-5 md:ml-20 lg:ml-30">
-        <table className="w-full table-fixed border-b-2 border-neutral-400">
-          <thead>
-            <tr className="text-black text-xs md:text-lg lg:text-2xl font-bold">
-              <td>ORDER ID</td>
-              <td>DATE</td>
-              <td>TOTAL (IDR)</td>
-              <td>ACTION</td>
-            </tr>
-          </thead>
-        </table>
-        <div className="grid gap-4">
-          {datas.map((data) => (
-            <AdminHO
-              key={data.orderid}
-              id={data.orderid}
-              date={data.created_at}
-              total={data.total}
-            />
-          ))}
+        <div className="ml-5 md:ml-20 lg:ml-30">
+          <table className="w-full table-fixed border-b-2 border-neutral-400">
+            <thead>
+              <tr className="text-black text-xs md:text-lg lg:text-2xl font-bold">
+                <td>ORDER ID</td>
+                <td>DATE</td>
+                <td>TOTAL (IDR)</td>
+                <td>ACTION</td>
+              </tr>
+            </thead>
+          </table>
+          <div className="grid gap-4">
+            {datas.map((data) => (
+              <AdminHO
+                key={data.orderid}
+                id={data.orderid}
+                date={data.created_at}
+                total={data.total}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default home_admin;

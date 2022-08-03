1d367554dd2cd9729c6ae5d6a1310d3db9a2e64d
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-03 19:45:44 +0700

    delete some unused comment texts

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,8 @@ export default function Home() {
   }, []);
 
   const fetchData = async () => {
-    // const myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "application/json");
-
-    // const raw = JSON.stringify({
-    //   name: "Cindy",
-    //   email: "mbacin@gmail.com",
-    //   password: 1234,
-    //   address: "Jln. Baru No.4, Jakarta - Indonesia",
-    //   phone: "08123247689",
-    // });
-
     const requestOptions = {
       method: "GET",
-      //   headers: myHeaders,
-      //   body: raw,
-      //   redirect: "follow",
     };
 
     fetch(

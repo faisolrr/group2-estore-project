8576fec2e3fb3d661fa2ba3ca47bf18f04fa3a81
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-03 10:27:46 +0700

    change from api placeholder to api swagger

--- pages/index.js
+++ pages/index.js
@@ -27,12 +27,15 @@ export default function Home() {
       //   redirect: "follow",
     };
 
-    fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
+    fetch(
+      "https://virtserver.swaggerhub.com/vaniliacahya/E-Store/1.0.0/products",
+      requestOptions
+    )
       .then((response) => response.json())
       .then((result) => {
         console.log(result);
         if (result) {
-          setDatas(result);
+          setDatas(result.data);
         }
       })
       .catch((error) => console.log("error", error));
@@ -41,16 +44,15 @@ export default function Home() {
     <div className="bg-[#eeee]">
       <div>Header</div>
       <div className="h-96 w-full bg-hero"></div>
-      <h1 className="text-center mt-6 p-0 md:p-12 lg:p-12 text-4xl lg:text-6xl md:text-6xl">
-        All Product
+      <h1 className="text-black text-center mt-6 p-0 md:p-12 lg:p-12 text-4xl lg:text-6xl md:text-6xl">
+        All Products
       </h1>
       <div className="m-10 gap-12 grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {datas.map((data) => (
           <DashboardCard
-            key={data.id}
-            title={data.title}
-            body={data.body}
-            price={data.id}
+            key={data.productid}
+            title={data.productname}
+            price={data.price}
           />
         ))}
       </div>

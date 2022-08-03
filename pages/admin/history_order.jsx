4df374f119676901f08f839b6b55f4c7b82b7c36
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-03 15:26:15 +0700

    slicing all users order history page and config api fetch data

--- pages/admin/history_order.jsx
+++ pages/admin/history_order.jsx
@@ -1,10 +1,87 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
-function history_order(props) {
-  return <div>history_order</div>;
+import Header from "../../components/Header";
+import { AdminHO } from "../../components/Card";
+
+function home_admin() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    // const raw = JSON.stringify({
+    //   name: "Cindy",
+    //   email: "mbacin@gmail.com",
+    //   password: 1234,
+    //   address: "Jln. Baru No.4, Jakarta - Indonesia",
+    //   phone: "08123247689",
+    // });
+
+    const requestOptions = {
+      method: "GET",
+      //   headers: myHeaders,
+      //   body: raw,
+      //   redirect: "follow",
+    };
+
+    fetch(
+      "https://virtserver.swaggerhub.com/vaniliacahya/E-Store/1.0.0/admins/history",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result) {
+          setDatas(result.data);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+  return (
+    <div className="h-screen w-screen bg-[#eeee]">
+      <Header />
+      <div className="my-10 md:my-14 lg:my-20 flex space-x-5 justify-center">
+        <h1 className="text-black text-center text-base font-bold md:text-4xl lg:text-4xl">
+          All Users Order History
+        </h1>
+        <Link href="./home_admin">
+          <button className="p-1 bg-[#557EBC] text-white rounded-md text-sm md:text-2xl lg:text-4xl">
+            Back
+          </button>
+        </Link>
+      </div>
+
+      <div className="ml-5 md:ml-20 lg:ml-30">
+        <table className="w-full table-fixed border-b-2 border-neutral-400">
+          <thead>
+            <tr className="text-black text-xs md:text-lg lg:text-2xl font-bold">
+              <td>ORDER ID</td>
+              <td>DATE</td>
+              <td>TOTAL (IDR)</td>
+              <td>ACTION</td>
+            </tr>
+          </thead>
+        </table>
+        <div className="grid gap-4">
+          {datas.map((data) => (
+            <AdminHO
+              key={data.orderid}
+              id={data.orderid}
+              date={data.created_at}
+              total={data.total}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-history_order.propTypes = {};
-
-export default history_order;
+export default home_admin;
